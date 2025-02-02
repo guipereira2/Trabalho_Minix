9cb962cf122b249a90e26f34df16dc4fb05c6667
GutscheWilly <willy.gutsche@unifesp.br>
2025-02-01 22:02:01 -0300

    política de escalonamento: FCFS

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -299,6 +299,10 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 	int err;
 	int new_prio, new_quantum, new_cpu, niced;
 
+	if (p->p_rts_flags == 0) { // Processo está pronto
+        pick_proc();  // Apenas troca se o processo atual terminar
+    }
+
 	pick_cpu(rmp);
 
 	if (flags & SCHEDULE_CHANGE_PRIO)
